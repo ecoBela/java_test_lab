0c7ef22dda5cc08e87e5b821085c206fd5aaa693
Bela <belahamid@gmail.com>
2021-02-15 16:38:10 +0000

    two tests passed

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,26 +1,24 @@
 public class Calculator {
 
-    private double number1;
-    private double number2;
 
-    public Calculator(double number1, double number2){
-        this.number1 = number1;
-        this.number2 = number2;
+
+    public Calculator(){
+
     }
 
-    public double add(){
+    public double add(double number1, double number2){
         return number1 + number2;
     }
 
-    public double substract() {
-        return this.number1 - this.number2;
+    public double substract(double number1, double number2) {
+        return number1 - number2;
     }
-
-    public double multiply(){
-        return this.number1 * this.number2;
-    }
-
-    public double divide(){
-        return this.number1 / this.number2;
-    }
+//
+//    public double multiply(){
+//        return this.number1 * this.number2;
+//    }
+//
+//    public double divide(){
+//        return this.number1 / this.number2;
+//    }
 }
--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -6,30 +6,34 @@ public class calculatorTest {
 
     Calculator calculator;
 
+    @Before
+    public void before(){
+        calculator = new Calculator();
+    }
+
+
 
     @Test
     public void add(){
-        calculator = new Calculator(3, 2);
-        assertEquals(5, this.calculator.add(), 0.01);
+        assertEquals(5, this.calculator.add(2, 3), 0.01);
     }
 
     @Test
-    public void substractsToOne(){
-        calculator = new Calculator(100, 99);
-        assertEquals(1, calculator.substract(),0.01);
+    public void substract(){
+        assertEquals(4, calculator.substract(6, 2),0.01);
     }
-
-    @Test
-    public void multiply(){
-        calculator = new Calculator(2, 3);
-        assertEquals(6, calculator.multiply(), 0.01);
-    }
-
-    @Test
-    public void divideToOneAndAHalf(){
-        calculator = new Calculator(3, 2);
-        assertEquals(1.5, calculator.divide(), 0.01);
-    }
+//
+//    @Test
+//    public void multiply(){
+//        calculator = new Calculator(2, 3);
+//        assertEquals(6, calculator.multiply(), 0.01);
+//    }
+//
+//    @Test
+//    public void divideToOneAndAHalf(){
+//        calculator = new Calculator(3, 2);
+//        assertEquals(1.5, calculator.divide(), 0.01);
+//    }
 
 
 
